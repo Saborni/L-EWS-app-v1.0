df02c3943edbe28d2a299c1b40c6555c72ef785d
Saborni <bhattacharyasab93@gmail.com>
2020-01-08 12:14:55 +0530

    5th commit

--- src/main/java/org/sab/controller/ThymeleafController.java
+++ src/main/java/org/sab/controller/ThymeleafController.java
@@ -3,12 +3,17 @@ package org.sab.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 @Controller
 @RequestMapping("/")
 public class ThymeleafController {
 
+    @RequestMapping(value={"/","/index.html","/error",},method = GET)
     public String returnLibHomePage(){
         return "index";
     }
 
+    @RequestMapping("/tutorials_spring.html")
+    public String returnSpringTutorials(){return "tutorials_spring";}
 }
